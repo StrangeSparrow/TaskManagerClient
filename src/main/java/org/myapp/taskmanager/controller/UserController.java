534a2301f0f692225954c74a2c9085c99e83ae97
StrangeSparrow <73070412+StrangeSparrow@users.noreply.github.com>
2021-01-11 20:41:04 +0300

    Add  user viwe

--- src/main/java/org/myapp/taskmanager/controller/UserController.java
+++ src/main/java/org/myapp/taskmanager/controller/UserController.java
@@ -11,6 +11,7 @@ import org.myapp.taskmanager.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -33,5 +34,14 @@ public class UserController {
         model.addAttribute("users", users);
 
         return "users";
+    }
+
+    @GetMapping("/{id}")
+    public String getUser(Model model, @PathVariable("id") String id) {
+        UserDto user = userClient.findById(id);
+
+        model.addAttribute("user", user);
+
+        return "user";
     }
 }
